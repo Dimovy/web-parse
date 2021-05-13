c9f341afcc476ec61e248b6eae972af280157fdc
Dimovy <>
2021-05-13 14:57:44 +0300

    add build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 function App() {
+
+  const url = 'https://gfs.gov.ru/poisk/?q=%D0%A0%D0%96%D0%94&s=+'
+  
+
+  axios.get(url)
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <span>Answer</span>
     </div>
   );
 }
